d0a1459f08e78f0bdcd5205962228e15aa0eda2c
VICTOR RICARDO CAETANO LIMA <victortrader770@gmail.com>
2022-03-08 19:28:40 -0300

    alterações feitas no curso

---
+++ javascript/aula012/ex011.js
@@ -0,0 +1,8 @@
+let idade = 22
+if (idade < 16) {
+    console.log('Vota!!')
+} else if (idade > 18) {
+    console.log('Voto opcional')
+}
+    
+
---
+++ javascript/aula012ex/ex014/script.js
@@ -0,0 +1,18 @@
+function carregar() {
+    var msg = document.querySelector('#msg');
+    var img = document.querySelector('#imagem');
+    var data = new Date();
+    var hora =  data.getHours();
+    // let hora = 3
+    msg.innerHTML = `Agora são ${hora} horas.` 
+    if(hora >= 0 && hora < 12 ) {
+        img.src = 'imagens/imagem-manha.jpg'
+        document.body.style.background = '#6E85A0'
+    } else if (hora >= 12 && hora < 18) {
+        img.src = 'imagens/imagem-tarde.jpg'
+        document.body.style.background = '#A65729'
+    } else {
+        img.src = 'imagens/imagem-noite.jpg'
+        document.body.style.background = '#A488BF'
+    }
+};
