b529c74088201ec69887d23a9aa3c68c47ac1aeb
VICTOR RICARDO CAETANO LIMA <victortrader770@gmail.com>
2022-03-16 16:06:27 -0300

    repeticoes em js

---
+++ javascript/aula013/ambiente.js
@@ -0,0 +1,17 @@
+var c = 1
+do {
+    console.log(`olá eu fui o numero = ${c}`)
+    c++
+}while (c <= 200)
+
+
+
+// var c = 1
+// while (c <= 20) {
+//     console.log(`KM ${c}`)
+//     c++
+// }
+
+
+// console.log('olá tudo bem ?')
+// console.log('olá tudo bem ?')
